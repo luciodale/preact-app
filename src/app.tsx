1e68b53b8ecd5fff6e859f8f72cbc3f3b889b85e
Lucio D'Alessandro <lda@juxt.pro>
2023-05-19 17:41:46 +0200

    Add axios and tidy up a bit

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,5 @@
-import { MutationCache, QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import {
-  PersistQueryClientProvider,
-  PersistedClient,
-  Persister
-} from '@tanstack/react-query-persist-client'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import {
   Link,
   Outlet,
@@ -14,69 +9,13 @@ import {
   RouterProvider
 } from '@tanstack/router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { del, get, set } from 'idb-keyval'
 import { StrictMode } from 'preact/compat'
-import toast, { Toaster } from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 import './app.css'
 import { GeoLocation } from './components/Geolocation'
 import Report from './components/Report'
 import { fetchData } from './queries'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
-      staleTime: 2000,
-      retry: 0
-    }
-  },
-  mutationCache: new MutationCache({
-    onSuccess: (data) => {
-      console.log('mutationCache.onSuccess', data)
-      toast.success(data)
-    },
-    onError: (error) => {
-      if (error instanceof Error) toast.error(error.message)
-    }
-  })
-})
-
-// we need a default mutation function so that paused mutations can resume after a page reload
-queryClient.setMutationDefaults(['mutation'], {
-  mutationFn: async ({ text }: { text: string }): Promise<string> => {
-    // to avoid clashes with our optimistic update when an offline mutation continues
-    await queryClient.cancelQueries({ queryKey: ['data'] })
-    return fetch('/api/data', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ text })
-    }).then((res) => res.json())
-  }
-})
-
-/**
- * Creates an Indexed DB persister
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
- */
-export function createIDBPersister(idbValidKey: IDBValidKey = 'tbSpecialist') {
-  return {
-    persistClient: async (client: PersistedClient) => {
-      set(idbValidKey, client)
-    },
-    restoreClient: async () => {
-      const client = await get<PersistedClient>(idbValidKey)
-      return client
-    },
-    removeClient: async () => {
-      await del(idbValidKey)
-    }
-  } as Persister
-}
-
-const persister = createIDBPersister()
+import { persister, queryClient } from './utils'
 
 const rootRoute = new RootRoute({
   component: () => (
---
+++ src/utils.ts
@@ -0,0 +1,58 @@
+import { MutationCache, QueryClient } from '@tanstack/react-query'
+import {
+  PersistedClient,
+  Persister
+} from '@tanstack/react-query-persist-client'
+import axios from 'axios'
+import { del, get, set } from 'idb-keyval'
+import toast from 'react-hot-toast'
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+      staleTime: 2000,
+      retry: false
+    }
+  },
+  mutationCache: new MutationCache({
+    onSuccess: (data) => {
+      console.log('mutationCache.onSuccess', data)
+      toast.success(data)
+    },
+    onError: (error) => {
+      if (error instanceof Error) toast.error(error.message)
+    }
+  })
+})
+
+/**
+ * Creates an Indexed DB persister
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
+ */
+export function createIDBPersister(idbValidKey: IDBValidKey = 'tbSpecialist') {
+  return {
+    persistClient: async (client: PersistedClient) => {
+      set(idbValidKey, client)
+    },
+    restoreClient: async () => {
+      const client = await get<PersistedClient>(idbValidKey)
+      return client
+    },
+    removeClient: async () => {
+      await del(idbValidKey)
+    }
+  } as Persister
+}
+
+export const persister = createIDBPersister()
+
+// we need a default mutation function so that paused mutations can resume after a page reload
+queryClient.setMutationDefaults(['mutation'], {
+  mutationFn: async ({ text }: { text: string }): Promise<string> => {
+    // to avoid clashes with our optimistic update when an offline mutation continues
+    await queryClient.cancelQueries({ queryKey: ['data'] })
+    return axios.post('/api/data', { text }).then((res) => res.data)
+  }
+})
